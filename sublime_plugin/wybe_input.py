16c522dc38a2dd1eab259932aabe8d2ceb76270a
Luis Ángel Méndez Gort <lamg@protonmail.com>
2024-05-11 10:56:10 +0200

    sublime text support

---
+++ sublime_plugin/wybe_input.py
@@ -0,0 +1,23 @@
+import sublime
+import sublime_plugin
+
+class ReplaceListener(sublime_plugin.EventListener):
+    def on_modified(self, view):
+        cursor_pos = view.sel()[0].begin()
+
+        last_two_chars = view.substr(sublime.Region(cursor_pos - 2, cursor_pos))
+        
+        sequences = {
+            '==': '≡',
+            '/=': '≢',
+            '&&': '∧',
+            '||': '∨',
+            '=>': '⇒',
+            '<=': '⇐',
+            '-,': '¬',
+            '|]': '□',
+        }
+
+        if last_two_chars in sequences:
+            view.run_command('undo')
+            view.run_command('insert', {'characters': sequences[last_two_chars]})
