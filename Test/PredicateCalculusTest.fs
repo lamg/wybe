896f7f7c442092d6a47af587a58ec93f06bfbd1b
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-05-24 13:08:48 +0200

    separate file for tests on functions

--- Test/PredicateCalculusTest.fs
+++ Test/PredicateCalculusTest.fs
@@ -28,44 +28,4 @@ let ``check all theorems`` () =
     ``∨ ∧ absorption`` ]
   |> findFailingProof
 
-open Core
-open GriesSchneider.Functions
-open GriesSchneider.Integers
 
-[<Fact>]
-let ``test fibonacci function`` () =
-  let two = Integer 2
-
-  proof {
-    lemma (fib two = fib one + fib zero)
-
-    fib two
-    ``==`` { fibProp }
-    fib one + fib zero
-  }
-  |> inspect
-  |> calculationError
-  |> print
-  |> ignore
-
-[<Fact>]
-let ``test fibonacci invariant`` () =
-  let i, a, b = mkIntVar "i", mkIntVar "a", mkIntVar "b"
-
-  // a = fibonacci (i - 1) ∧ b = fibonacci i ⇒ a + b = fibonacci (i + 1)
-  proof { lemma (a = fib (i - one) <&&> (b = fib i) ==> (a + b = fib (i + one))) }
-  |> inspect
-  |> calculationError
-  |> print
-
-[<Fact>]
-let ``test factorial invariant`` () =
-  // result = factorial i ⇒ result * i = factorial (i + 1)
-  let i = mkIntVar "i"
-  let result = mkIntVar "result"
-
-  // result = factorial i ⇒ result * i = factorial (i + 1)
-  proof { lemma (result = fact i ==> (result * i = fact (i + one))) }
-  |> inspect
-  |> calculationError
-  |> print
