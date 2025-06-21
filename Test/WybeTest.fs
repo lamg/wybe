e4f0b18000bdcf2990ad8845046131cd959d219a
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-06-21 13:19:54 +0200

    typing x div y test passing

--- Test/WybeTest.fs
+++ Test/WybeTest.fs
@@ -1,13 +1,19 @@
 module WybeTest
 
 open Xunit
+open FsUnitTyped
 open LanguageServices.Wybe
 open AST
 open Semantics
 
 [<Fact>]
-let ``typing simple expression`` () =
+let ``typing x div y`` () =
   let vars = [ "x", WybeType.Integer; "y", WybeType.Integer ] |> Map.ofList
   let divVars = Binary(Var "x", WybeOp.Div, Var "y")
   let r = extractSemantics vars divVars
-  printfn $"{exprToTree r.Expr} {r.SemanticResult.Type} {r.SemanticResult.Domain |> Option.map (_.Expr >> exprToTree)}"
+  shouldEqual "x ÷ y" (r.Expr |> exprToTree |> string)
+  shouldEqual (Some WybeType.Integer) r.SemanticResult.Type
+  Assert.True r.SemanticResult.Domain.IsSome
+  let domain = r.SemanticResult.Domain.Value
+  shouldEqual (Some WybeType.Boolean) domain.SemanticResult.Type
+  shouldEqual "y ≠ 0" (domain.Expr |> exprToTree |> string)
